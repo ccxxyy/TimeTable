115a11ac0065f633c494fbdc6ab0fde017839c36
Xiaoyi Cai <sawyer_cai@Xiaoyis-MacBook-Pro.local>
2011-11-29 22:19:12 +0000

    Initial Commit

---
+++ TimeTable/TimeTableAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TimeTableAppDelegate.h
+//  TimeTable
+//
+//  Created by Xiaoyi Cai on 29/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimeTableAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TimeTable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimeTable
+//
+//  Created by Xiaoyi Cai on 29/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TimeTableAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TimeTableAppDelegate class]));
+    }
+}
